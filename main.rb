5e962d9174380f5f41341351163f680bd80ae1c3
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-27 20:05:56 -0600

    Feat: add logic to while loop/

--- main.rb
+++ main.rb
@@ -6,3 +6,9 @@ number = gets.chomp.to_i
 fibonacciPrev = 0
 fibonacci = 1
 
+while fibonacci <= number
+  puts fibonacci
+  temp = fibonacci
+  fibonacci = fibonacci + fibonacciPrev
+  fibonacciPrev = temp
+end
